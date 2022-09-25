5980039515413c3bdccfd67135cff45858fd4bcc
Alexander <gallade2014@gmail.com>
2022-09-24 23:30:19 -0600

    Added some sections.

---
+++ src/sections/IngredientsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function IngredientsSection(props) {
+    return (
+        <div>
+            <h1>Ingredients</h1>
+        </div>
+    );
+}
+
+export default IngredientsSection;
---
+++ src/sections/RecipesSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecipesList from '../components/RecipesList';
+
+function RecipesSection() {
+    return (
+        <div>
+            <h1>Recipes</h1>
+            <RecipesList />
+        </div>
+    );
+}
+
+export default RecipesSection;
