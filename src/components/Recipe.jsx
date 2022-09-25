386ecd758287d7283ccb760715d73c118424ecdf
Alexander <gallade2014@gmail.com>
2022-09-24 20:59:59 -0600

    Finished recipe component.

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Ingredient from './Ingredient';
+import Tag from './Tag';
 
 function Recipe(props) {
 
@@ -9,11 +10,19 @@ function Recipe(props) {
         <div className='container mt-4'>
             <h1>{recipe?.name}</h1>
             <h2>{recipe?.steps}</h2>
-            <img src={`http://127.0.0.1:8000/api/image/recipe/${recipe?.image.image}`} alt={recipe?.name + " image"} />
+            <h3>{recipe?.category?.name}</h3>
+            <img src={`http://127.0.0.1:8000/api/image/recipe/${recipe?.image.image}`} alt={recipe?.name + " image"} style={{ width: "350px" }} />
             <div className='row row-cols-1 row-cols-md-3 g-4'>
                 {
                     recipe?.ingredients.map((ingredient, idx) => {
                         return <Ingredient key={idx} ingredient={ingredient} />
+                    })
+                }
+            </div>
+            <div>
+                {
+                    recipe?.tags.map((tag) => {
+                        return <Tag name={tag.name} />
                     })
                 }
             </div>
