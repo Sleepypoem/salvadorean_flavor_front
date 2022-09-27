62982cdfc9af783a7e55ca5b906fb3964f069e29
Alexander <gallade2014@gmail.com>
2022-09-26 21:24:46 -0600

    Navbar now hides on login screen.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,46 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from "react-router-dom";
+import { UserContext } from '../context/UserContextProvider';
 
 
 function NavBar() {
+    const { token } = useContext(UserContext);
     return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <div className="container">
-                <Link className="navbar-brand" href="/">Salvadorean Flavor</Link>
-                <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" to="/" aria-current="page">Home <span className="visually-hidden">(current)</span></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/recipes">Recipes</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/ingredients">Ingredients</Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</Link>
-                            <div className="dropdown-menu" aria-labelledby="dropdownId">
-                                <Link className="dropdown-item" to="/categories">Action 1</Link>
-                                <Link className="dropdown-item" href="#">Action 2</Link>
-                            </div>
-                        </li>
-                    </ul>
-                    <form className="d-flex my-2 my-lg-0">
-                        <input className="form-control me-sm-2" type="text" placeholder="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
+        token === null ? null :
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <div className="container">
+                    <Link className="navbar-brand" href="/">Salvadorean Flavor</Link>
+                    <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/" aria-current="page">Home <span className="visually-hidden">(current)</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/recipes">Recipes</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/ingredients">Ingredients</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categories</Link>
+                                <div className="dropdown-menu" aria-labelledby="dropdownId">
+                                    <Link className="dropdown-item" to="/categories">Action 1</Link>
+                                    <Link className="dropdown-item" href="#">Action 2</Link>
+                                </div>
+                            </li>
+                        </ul>
+                        <form className="d-flex my-2 my-lg-0">
+                            <input className="form-control me-sm-2" type="text" placeholder="Search" />
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
 
     );
 }
