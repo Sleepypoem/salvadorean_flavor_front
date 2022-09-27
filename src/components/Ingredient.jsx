5db5265af4f4f547d34f242c55ca87f9d7a158ec
glendagarcia99 <jeanmilletteg99@gmail.com>
2022-09-27 00:23:18 -0600

    Vista de recetas

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Recipe.css';
 
 function Ingredient(props) {
 
@@ -9,9 +10,10 @@ function Ingredient(props) {
             <div className="card h-100">
 
                 <div className="card-body">
-                    <h3 className="card-title">{ingredient?.name}</h3>
-                    <img src={`http://127.0.0.1:8000/api/image/ingredient/${ingredient?.image.image}`} className="img-fluid rounded-start" alt={ingredient?.name + " image."} style={{ width: "350px" }} />
+                    <img src={`http://127.0.0.1:8000/api/image/ingredient/${ingredient?.image.image}`} className="image-ing" alt={ingredient?.name + " image."} style={{ width: "200px" }} />
+                    <h5 className="card-title">{ingredient?.name}</h5>
                 </div>
+
                 <div className="card-footer">
                     <small className="text-muted">Last updated 3 mins ago</small>
                 </div>
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -2,57 +2,46 @@
 import React from 'react';
 import Ingredient from './Ingredient';
 import Tag from './Tag';
+import './Recipe.css';
 
 function Recipe(props) {
 
     const { recipe } = props;
 
     return (
-        <section className='containerRecipe'>
-            <section className='containerName'>
-            <h1>{recipe?.name}</h1>
-            </section>
-             <section className='containerRecipe'>
-             <div>
-             <img className='imgRecipe' src={`http://127.0.0.1:8000/api/image/recipe/${recipe?.image.image}`} alt={recipe?.name + " image"} style={{ width: "600px" }} />
-             </div>
-             <section>
-            <br>
-            </br>
-             </section>
-             <div className='ImagesIngredients'>
-                {  
-                   
-                    recipe?.ingredients.map((ingredient, idx) => {
-                        return <Ingredient key={idx} ingredient={ingredient} classname='imgingredient' />
-                    })
-                    
-                    
-                }
-            </div>
-             </section>
-             <section className='containerSteps'>
-                <div>
-                <h2>{recipe?.steps}</h2>
-                </div>
-             
-             </section>
-            <section>
-            <h3>{recipe?.category?.name}</h3> 
-            </section>
-            <section>
-            <div>
+        <section className='container mt-4'>
+            
+                <h3>{recipe?.category?.name}</h3> 
+                <h1>{recipe?.name}</h1>
+                <section className='recipe'>
+                    <img className='imgRecipe' src={`http://127.0.0.1:8000/api/image/recipe/${recipe?.image.image}`} alt={recipe?.name + " image"} style={{ width: "600px" }} />    
+                </section>  
+                <section className='bck'>
+                <h2>Ingredients</h2>
+                <section className='ImagesIngredients'>  
+                    {  
+                        recipe?.ingredients.map((ingredient, idx) => {
+                            return <Ingredient key={idx} ingredient={ingredient}/>}) 
+                    }
+                </section> 
+                <section className='steps'>
+                    <h2>Steps</h2>
+                    <p>{recipe?.steps}</p>
+                </section>
+                </section>
+                <section className='tags'>
                 {
                     recipe?.tags.map((tag) => {
                         return <Tag name={tag.name} />
                     })
                 }
-            </div>
-
-            </section>
-            
-
+                </section>
+                
+               
         </section>
+        
+            
+            
     );
 
 }
