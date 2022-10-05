03aa6663a011d3a95c5fc025ed8e6812c390ecb1
Alexander <gallade2014@gmail.com>
2022-10-04 22:47:57 -0600

    Ingredient now shows recipes relationship

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import { RESPONSIVE_SETTINGS } from '../utils/Carousel_Settings';
 import { IMAGE_URL } from '../utils/Links';
 import './Recipe.css';
+import RecipeCard from './RecipeCard';
 
 function Ingredient(props) {
 
@@ -12,13 +16,19 @@ function Ingredient(props) {
 
                 <div className="card-body">
                     <img src={`${IMAGE_URL}ingredient/${ingredient?.image?.image}`} className="image-ing mx-auto" alt={ingredient?.name + " image."} style={{ width: "200px", height: "200px" }} />
-                    <h5 className="card-title">{ingredient?.name}</h5>
+                    <h5 className="card-title"><Link to={`/ingredient/${ingredient?.ingredient_id}`}>{ingredient?.name}</Link></h5>
                 </div>
 
                 <div className="card-footer">
-                    <small className="text-muted">Last updated 3 mins ago</small>
                 </div>
             </div>
+
+            <Slider {...RESPONSIVE_SETTINGS} >
+                {
+                    ingredient?.recipes?.map((recipe, idx) => {
+                        return <RecipeCard key={idx} name={recipe.name} image={recipe?.image?.image} id={recipe?.recipe_id} />
+                    })
+                }</Slider>
         </div>
     );
 }
